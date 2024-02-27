3bb21a2a18d62b0da9d0d1c73865e1eb74335866
Rainolds Rusiņš <rr2519@gymn.local>
2024-02-27 15:00:18 +0200

    Otrā augšupielāde

--- RR_PD_v6/Form1.Designer.cs
+++ RR_PD_v6/Form1.Designer.cs
@@ -28,13 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_virsraksts = new System.Windows.Forms.Label();
+            this.txt_virsraksts2 = new System.Windows.Forms.Label();
+            this.txt_virsraksts3 = new System.Windows.Forms.Label();
+            this.txt_virsraksts4 = new System.Windows.Forms.Label();
+            this.txtbox_prece1 = new System.Windows.Forms.TextBox();
+            this.txtbox_prece2 = new System.Windows.Forms.TextBox();
+            this.txtbox_prece3 = new System.Windows.Forms.TextBox();
+            this.txtbox_cena1 = new System.Windows.Forms.TextBox();
+            this.txtbox_cena2 = new System.Windows.Forms.TextBox();
+            this.txtbox_cena3 = new System.Windows.Forms.TextBox();
+            this.num_daudzums1 = new System.Windows.Forms.NumericUpDown();
+            this.num_daudzums2 = new System.Windows.Forms.NumericUpDown();
+            this.num_daudzums3 = new System.Windows.Forms.NumericUpDown();
+            this.txt_virsraksts5 = new System.Windows.Forms.Label();
+            this.txtbox_maks = new System.Windows.Forms.TextBox();
+            this.btn_aprekins = new System.Windows.Forms.Button();
+            this.txt_virsraksts6 = new System.Windows.Forms.Label();
+            this.txt_virsraksts7 = new System.Windows.Forms.Label();
+            this.txtbox_summa = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_virsraksts
+            // 
+            this.txt_virsraksts.AutoSize = true;
+            this.txt_virsraksts.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txt_virsraksts.Location = new System.Drawing.Point(85, 56);
+            this.txt_virsraksts.Name = "txt_virsraksts";
+            this.txt_virsraksts.Size = new System.Drawing.Size(385, 18);
+            this.txt_virsraksts.TabIndex = 0;
+            this.txt_virsraksts.Text = "Ievadiet izvēlētās preces nosaukumu, cenu un daudzumu!";
+            // 
+            // txt_virsraksts2
+            // 
+            this.txt_virsraksts2.AutoSize = true;
+            this.txt_virsraksts2.Location = new System.Drawing.Point(88, 95);
+            this.txt_virsraksts2.Name = "txt_virsraksts2";
+            this.txt_virsraksts2.Size = new System.Drawing.Size(97, 13);
+            this.txt_virsraksts2.TabIndex = 1;
+            this.txt_virsraksts2.Text = "Preces nosaukums";
+            // 
+            // txt_virsraksts3
+            // 
+            this.txt_virsraksts3.AutoSize = true;
+            this.txt_virsraksts3.Location = new System.Drawing.Point(246, 95);
+            this.txt_virsraksts3.Name = "txt_virsraksts3";
+            this.txt_virsraksts3.Size = new System.Drawing.Size(67, 13);
+            this.txt_virsraksts3.TabIndex = 2;
+            this.txt_virsraksts3.Text = "Preces cena";
+            // 
+            // txt_virsraksts4
+            // 
+            this.txt_virsraksts4.AutoSize = true;
+            this.txt_virsraksts4.Location = new System.Drawing.Point(387, 95);
+            this.txt_virsraksts4.Name = "txt_virsraksts4";
+            this.txt_virsraksts4.Size = new System.Drawing.Size(57, 13);
+            this.txt_virsraksts4.TabIndex = 3;
+            this.txt_virsraksts4.Text = "Daudzums";
+            // 
+            // txtbox_prece1
+            // 
+            this.txtbox_prece1.Location = new System.Drawing.Point(88, 112);
+            this.txtbox_prece1.Name = "txtbox_prece1";
+            this.txtbox_prece1.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_prece1.TabIndex = 4;
+            // 
+            // txtbox_prece2
+            // 
+            this.txtbox_prece2.Location = new System.Drawing.Point(88, 139);
+            this.txtbox_prece2.Name = "txtbox_prece2";
+            this.txtbox_prece2.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_prece2.TabIndex = 5;
+            // 
+            // txtbox_prece3
+            // 
+            this.txtbox_prece3.Location = new System.Drawing.Point(88, 166);
+            this.txtbox_prece3.Name = "txtbox_prece3";
+            this.txtbox_prece3.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_prece3.TabIndex = 6;
+            // 
+            // txtbox_cena1
+            // 
+            this.txtbox_cena1.Location = new System.Drawing.Point(249, 112);
+            this.txtbox_cena1.Name = "txtbox_cena1";
+            this.txtbox_cena1.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_cena1.TabIndex = 7;
+            // 
+            // txtbox_cena2
+            // 
+            this.txtbox_cena2.Location = new System.Drawing.Point(249, 139);
+            this.txtbox_cena2.Name = "txtbox_cena2";
+            this.txtbox_cena2.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_cena2.TabIndex = 8;
+            // 
+            // txtbox_cena3
+            // 
+            this.txtbox_cena3.Location = new System.Drawing.Point(249, 166);
+            this.txtbox_cena3.Name = "txtbox_cena3";
+            this.txtbox_cena3.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_cena3.TabIndex = 9;
+            // 
+            // num_daudzums1
+            // 
+            this.num_daudzums1.Location = new System.Drawing.Point(390, 112);
+            this.num_daudzums1.Name = "num_daudzums1";
+            this.num_daudzums1.Size = new System.Drawing.Size(120, 20);
+            this.num_daudzums1.TabIndex = 10;
+            // 
+            // num_daudzums2
+            // 
+            this.num_daudzums2.Location = new System.Drawing.Point(390, 139);
+            this.num_daudzums2.Name = "num_daudzums2";
+            this.num_daudzums2.Size = new System.Drawing.Size(120, 20);
+            this.num_daudzums2.TabIndex = 11;
+            // 
+            // num_daudzums3
+            // 
+            this.num_daudzums3.Location = new System.Drawing.Point(390, 165);
+            this.num_daudzums3.Name = "num_daudzums3";
+            this.num_daudzums3.Size = new System.Drawing.Size(120, 20);
+            this.num_daudzums3.TabIndex = 12;
+            // 
+            // txt_virsraksts5
+            // 
+            this.txt_virsraksts5.AutoSize = true;
+            this.txt_virsraksts5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txt_virsraksts5.Location = new System.Drawing.Point(88, 215);
+            this.txt_virsraksts5.Name = "txt_virsraksts5";
+            this.txt_virsraksts5.Size = new System.Drawing.Size(245, 18);
+            this.txt_virsraksts5.TabIndex = 13;
+            this.txt_virsraksts5.Text = "Ievadiet kāda summa Jums ir makā.";
+            // 
+            // txtbox_maks
+            // 
+            this.txtbox_maks.Location = new System.Drawing.Point(91, 237);
+            this.txtbox_maks.Name = "txtbox_maks";
+            this.txtbox_maks.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_maks.TabIndex = 14;
+            // 
+            // btn_aprekins
+            // 
+            this.btn_aprekins.Location = new System.Drawing.Point(88, 282);
+            this.btn_aprekins.Name = "btn_aprekins";
+            this.btn_aprekins.Size = new System.Drawing.Size(103, 42);
+            this.btn_aprekins.TabIndex = 15;
+            this.btn_aprekins.Text = "Aprēķināt summu";
+            this.btn_aprekins.UseVisualStyleBackColor = true;
+            this.btn_aprekins.Click += new System.EventHandler(this.btn_aprekins_Click);
+            // 
+            // txt_virsraksts6
+            // 
+            this.txt_virsraksts6.AutoSize = true;
+            this.txt_virsraksts6.Location = new System.Drawing.Point(88, 331);
+            this.txt_virsraksts6.Name = "txt_virsraksts6";
+            this.txt_virsraksts6.Size = new System.Drawing.Size(48, 13);
+            this.txt_virsraksts6.TabIndex = 16;
+            this.txt_virsraksts6.Text = "Summa: ";
+            // 
+            // txt_virsraksts7
+            // 
+            this.txt_virsraksts7.AutoSize = true;
+            this.txt_virsraksts7.Location = new System.Drawing.Point(88, 358);
+            this.txt_virsraksts7.Name = "txt_virsraksts7";
+            this.txt_virsraksts7.Size = new System.Drawing.Size(100, 13);
+            this.txt_virsraksts7.TabIndex = 17;
+            this.txt_virsraksts7.Text = "Vai pietiek naudas?";
+            // 
+            // txtbox_summa
+            // 
+            this.txtbox_summa.Location = new System.Drawing.Point(195, 331);
+            this.txtbox_summa.Name = "txtbox_summa";
+            this.txtbox_summa.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_summa.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(195, 358);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtbox_summa);
+            this.Controls.Add(this.txt_virsraksts7);
+            this.Controls.Add(this.txt_virsraksts6);
+            this.Controls.Add(this.btn_aprekins);
+            this.Controls.Add(this.txtbox_maks);
+            this.Controls.Add(this.txt_virsraksts5);
+            this.Controls.Add(this.num_daudzums3);
+            this.Controls.Add(this.num_daudzums2);
+            this.Controls.Add(this.num_daudzums1);
+            this.Controls.Add(this.txtbox_cena3);
+            this.Controls.Add(this.txtbox_cena2);
+            this.Controls.Add(this.txtbox_cena1);
+            this.Controls.Add(this.txtbox_prece3);
+            this.Controls.Add(this.txtbox_prece2);
+            this.Controls.Add(this.txtbox_prece1);
+            this.Controls.Add(this.txt_virsraksts4);
+            this.Controls.Add(this.txt_virsraksts3);
+            this.Controls.Add(this.txt_virsraksts2);
+            this.Controls.Add(this.txt_virsraksts);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_daudzums3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txt_virsraksts;
+        private System.Windows.Forms.Label txt_virsraksts2;
+        private System.Windows.Forms.Label txt_virsraksts3;
+        private System.Windows.Forms.Label txt_virsraksts4;
+        private System.Windows.Forms.TextBox txtbox_prece1;
+        private System.Windows.Forms.TextBox txtbox_prece2;
+        private System.Windows.Forms.TextBox txtbox_prece3;
+        private System.Windows.Forms.TextBox txtbox_cena1;
+        private System.Windows.Forms.TextBox txtbox_cena2;
+        private System.Windows.Forms.TextBox txtbox_cena3;
+        private System.Windows.Forms.NumericUpDown num_daudzums1;
+        private System.Windows.Forms.NumericUpDown num_daudzums2;
+        private System.Windows.Forms.NumericUpDown num_daudzums3;
+        private System.Windows.Forms.Label txt_virsraksts5;
+        private System.Windows.Forms.TextBox txtbox_maks;
+        private System.Windows.Forms.Button btn_aprekins;
+        private System.Windows.Forms.Label txt_virsraksts6;
+        private System.Windows.Forms.Label txt_virsraksts7;
+        private System.Windows.Forms.TextBox txtbox_summa;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RR_PD_v6/Form1.cs
+++ RR_PD_v6/Form1.cs
@@ -16,5 +16,23 @@ namespace RR_PD_v6
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_aprekins_Click(object sender, EventArgs e)
+        {
+            double cena1 = Convert.ToDouble(txtbox_cena1.Text);
+            double cena2 = Convert.ToDouble(txtbox_cena2.Text);
+            double cena3 = Convert.ToDouble(txtbox_cena3.Text);
+
+            int daudzums1 = Convert.ToInt32(num_daudzums1.Text);
+            int daudzums2 = Convert.ToInt32(num_daudzums2.Text);
+            int daudzums3 = Convert.ToInt32(num_daudzums3.Text);
+
+            double makaa = Convert.ToDouble(txtbox_maks.Text);
+        }
     }
 }
